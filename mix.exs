71a043f452d2eb96b45d7468fd934dd842b8dd5a
Marcin Operacz <marcin.operacz@gmail.com>
2016-10-28 14:43:49 +0200

    init

---
+++ mix.exs
@@ -0,0 +1,40 @@
+defmodule NervesSystemEvoklyThing.Mixfile do
+  use Mix.Project
+
+  @version Path.join(__DIR__, "VERSION")
+    |> File.read!
+    |> String.strip
+
+  def project do
+    [app: :nerves_system_evokly_thing,
+     version: @version,
+     elixir: "~> 1.2",
+     compilers: Mix.compilers ++ [:nerves_system],
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  def application do
+   []
+  end
+
+  defp deps do
+    [{:nerves_system, "~> 0.1.5"},
+     {:nerves_system_br, "~> 0.7.0"},
+     {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.7.0"}]
+  end
+
+  defp description do
+   """
+   Nerves System - Raspberry Pi 3 B
+   """
+  end
+
+  defp package do
+   [maintainers: ["Frank Hunleth", "Justin Schneck"],
+    files: ["LICENSE", "mix.exs", "nerves_defconfig", "nerves.exs", "README.md", "VERSION", "rootfs-additions", "fwup.conf", "cmdline.txt", "linux-4.4.defconfig", "config.txt", "post-createfs.sh"],
+    licenses: ["Apache 2.0"],
+    links: %{"Github" => "https://github.com/nerves-project/nerves_system_rpi3"}]
+  end
+end
