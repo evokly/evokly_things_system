bf90abc2291ecee39aefc45789e84cf01d0d30e2
Marcin Operacz <marcin.operacz@gmail.com>
2016-10-28 14:43:49 +0200

    init

---
+++ nerves.exs
@@ -0,0 +1,25 @@
+use Mix.Config
+
+version =
+  Path.join(__DIR__, "VERSION")
+  |> File.read!
+  |> String.strip
+
+config :nerves_system_evokly_thing, :nerves_env,
+  type: :system,
+  version: version,
+  mirrors: [
+    "https://github.com/evokly/evokly_things_system/releases/download/v#{version}/nerves_system_evokly_thing-v#{version}.tar.gz"
+    ],
+  build_platform: Nerves.System.Platforms.BR,
+  build_config: [
+    defconfig: "nerves_defconfig",
+    package_files: [
+      "rootfs-additions",
+      "linux-4.4.defconfig",
+      "fwup.conf",
+      "cmdline.txt",
+      "config.txt",
+      "post-createfs.sh"
+    ]
+  ]
